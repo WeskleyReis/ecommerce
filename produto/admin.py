64515402e7fc325bf670d58524f738535aa641c1
WeskleyReis <weskleyff88004216@gmail.com>
2025-07-22 21:29:46 -0300

    Iniciando projeto Ecommerce, models produto prontos.

---
+++ produto/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Produto, Variacao
+
+
+admin.site.register(Variacao)
+
+class VariacaoInLine(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [VariacaoInLine]
